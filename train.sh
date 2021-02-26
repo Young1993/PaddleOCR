1b7e0a52004f05ba539db2986b824cd92c76c973
wuyangjun <1090919981@qq.com>
2021-02-26 17:59:05 +0800

    train resnet50

--- train.sh
+++ train.sh
@@ -1,2 +1,6 @@
 # recommended paddle.__version__ == 2.0.0
-python3 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/rec/rec_mv3_none_bilstm_ctc.yml
+# python3 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/rec/rec_mv3_none_bilstm_ctc.yml
+# ZJ
+python3 -m paddle.distributed.launch tools/train.py -c configs/det/det_r50_vd_db.yml -o Global.pretrain_weights=./pretrain_models/det_r50_vd_db_v2.0_train/
+
+#FLAGS_selected_gpus=0,1,2 python3.5 tools/train.py -c configs/det/ch_ppocr_v2.0/ch_det_res18_db_v2.0.yml -o Global.pretrain_weights=./pretrain_models/ResNet18_vd_pretrained/
